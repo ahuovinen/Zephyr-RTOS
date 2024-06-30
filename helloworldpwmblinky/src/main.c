8eff2a1a22eee68c443cd4413a17049db68f12a1
Antti Huovinen <ahuovinen@gmail.com>
2024-06-30 13:00:02 +0000

    Led blinking. With pwm modulation.

--- helloworldpwmblinky/src/main.c
+++ helloworldpwmblinky/src/main.c
@@ -1,11 +1,18 @@
 #include <zephyr/device.h>
 #include <zephyr/drivers/pwm.h>
+#include <zephyr/kernel.h>
+
 
 static const struct pwm_dt_spec pwmled = 
     PWM_DT_SPEC_GET(DT_NODELABEL(pwm_led0));
 
 int main(void)
 {
-    
+    while(true) {
+        pwm_set_pulse_dt(&pwmled, 0);
+        k_msleep(1000);
+        pwm_set_pulse_dt(&pwmled, 10000);
+        k_msleep(1000);
+    }
     return 0;  
 }
